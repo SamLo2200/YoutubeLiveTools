5bd4db175543c775d746273be3f2f9dd7991a958
Sam <144938568+SamLo2200@users.noreply.github.com>
2024-05-04 08:33:24 +0800

    removed console.log in youtube api library

--- lib/youtube_api.ts
+++ lib/youtube_api.ts
@@ -1,7 +1,7 @@
 "use server";
 
 export default async function getVideoInfo(youtube_video_id: string) {
-    console.log("executed getVideoInfo");
+    // console.log("executed getVideoInfo");
     try {
         const response = await fetch(
             `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2C%20liveStreamingDetails&id=${youtube_video_id}&key=${process.env.API_KEY}`
