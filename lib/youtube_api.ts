25748ac912e7650059a87cd0e7799c69e08592aa
Sam <144938568+SamLo2200@users.noreply.github.com>
2024-05-05 10:37:27 +0800

    removed testing code

--- lib/youtube_api.ts
+++ lib/youtube_api.ts
@@ -21,15 +21,15 @@ interface YoutubeError {
 export default async function getVideoInfo(youtube_video_id: string) {
     // console.log("executed getVideoInfo");
     try {
-        // const response = await fetch(
-        //     `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2C%20liveStreamingDetails&id=${youtube_video_id}&key=${process.env.API_KEY}`
-        // );
+        const response = await fetch(
+            `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2C%20liveStreamingDetails&id=${youtube_video_id}&key=${process.env.API_KEY}`
+        );
 
         // Error testing
 
-        const response = await fetch(
-            `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2C%20liveStreamingDetails&id=${youtube_video_id}s&key=${process.env.API_KEY}`
-        );
+        // const response = await fetch(
+        //     `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2C%20liveStreamingDetails&id=${youtube_video_id}s&key=${process.env.API_KEY}`
+        // );
 
         if (!response.ok) {
             response.json().then((json: any) => {
